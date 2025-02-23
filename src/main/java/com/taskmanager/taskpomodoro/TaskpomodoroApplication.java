c97280afd1c90bd53e00b73feff272d0e0b3dc68
Hugo Saladini <hg.saladini@gmail.com>
2025-02-22 22:09:07 -0300

    Starting project, saving spring dependencies

---
+++ src/main/java/com/taskmanager/taskpomodoro/TaskpomodoroApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanager.taskpomodoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskpomodoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskpomodoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskmanager/taskpomodoro/TaskpomodoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmanager.taskpomodoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskpomodoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
