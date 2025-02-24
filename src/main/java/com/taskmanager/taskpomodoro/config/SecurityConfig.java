d62bf904b698745af61ae93e704e107cdc1e9fd4
Hugo Saladini <hg.saladini@gmail.com>
2025-02-24 18:47:29 -0300

    back end finished

---
+++ src/main/java/com/taskmanager/taskpomodoro/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.taskmanager.taskpomodoro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()  // Permite acesso sem login
+                )
+                .csrf().disable();  // Desativa proteção CSRF (caso necessário)
+
+        return http.build();
+    }
+}
+
